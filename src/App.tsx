463bc1820a603837a1002699a780d1e71e7c16eb
wywang <727936199@qq.com>
2021-01-13 18:21:15 +0800

    配置路由，完成基本导航栏功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
-
+import Money from 'views/Money'
+import {HashRouter as Router,Switch,Route,Link} from 'react-router-dom'
+import Statistics from './views/Statistics';
+import Tags from './views/Tags';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/money">money</Link>
+              </li>
+              <li>
+                <Link to="/tags">tags</Link>
+              </li>
+              <li>
+                <Link to="/statistics">statictis</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Route path="/">
+              <Money />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
