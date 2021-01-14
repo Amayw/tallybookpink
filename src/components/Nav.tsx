c5bad3224407189ee5a37bc6b2e4434a86d537d0
wywang <727936199@qq.com>
2021-01-14 14:40:13 +0800

    封装Icon组件，并在Nav导航栏中添加图标

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+import Icon from './Icon';
+
 
 const NavWrapper=styled.nav`
   >ul{
@@ -8,7 +10,17 @@ const NavWrapper=styled.nav`
     flex-direction: row;
     align-items: center;
     justify-content: space-around;
-    padding: 16px;
+    >li{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      padding: 6px;
+      >.icon{
+        width: 26px;
+        height: 26px;
+      }
+    }
   }
   box-shadow:0 0 3px rgba(0,0,0,0.25);
 `
@@ -17,12 +29,15 @@ export default function Nav(){
     <NavWrapper>
       <ul>
         <li>
+          <Icon name='rtag'/>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <Icon name='rmoney'/>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <Icon name='chart' />
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
